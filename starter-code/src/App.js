9e92c79674b34905191f1b7f7292349c40969423
Henrique Guazzelli Mendes <henriqueguazzellimendes@Henriques-MBP.localdomain>
2019-11-07 22:39:48 -0300

    lab done in code dojo

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Beers from './components/pages/Beers/Beers';
+import BeerDetail from './components/pages/BeerDetail/BeerDetail';
+import NewBeer from './components/pages/NewBeer/NewBeer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/beers" component={Beers} />
+          <Route exact path="/beers/:beerId" component={BeerDetail} />
+          <Route exact path="/new-beer" component={NewBeer} />
+        </Switch>
       </div>
     );
   }
